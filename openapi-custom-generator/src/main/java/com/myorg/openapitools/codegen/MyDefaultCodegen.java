a95203849dade8da05b5d30c5283351e754e5521
Your Name <you@example.com>
2023-03-06 22:51:16 +0900

    step7:http_methodの切り出し成功

--- openapi-custom-generator/src/main/java/com/myorg/openapitools/codegen/MyDefaultCodegen.java
+++ openapi-custom-generator/src/main/java/com/myorg/openapitools/codegen/MyDefaultCodegen.java
@@ -5832,7 +5832,7 @@ public class MyDefaultCodegen implements CodegenConfig {
     @Override
     public String apiFilename_http_method(String templateName, String tag, String http_method) {
         String suffix = apiTemplateFiles().get(templateName);
-        return apiFileFolder() + File.separator + toApiFilename(tag) + suffix;
+        return apiFileFolder() + File.separator + tag.toLowerCase() +  File.separator + toApiFilename(tag) + suffix;
     }
 
 
